6fc4c92363b6e1bb2f9d87be2a4371d7f6f1d4f9
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2024-10-26 13:25:38 -0300

    Update fatorial.py

--- funcoes.py/fatorial.py
+++ funcoes.py/fatorial.py
@@ -1,8 +1,21 @@
-def fatorial(num = 1):
+def fatorial(n, show=False):
+    """
+    Função para calcular o fatorial de um número
+    Param n: número a ter o seu fatorial calculado.
+    Comando show: mostra o cálculo do fatorial do número.
+    Por:
+    Gabriel de A. Vasconcelos
+    """
     f = 1
-    for c in range(num, 0, -1):
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
         f *= c
     return f
 
-n = int(input('Digite um número: '))
-print(f'O fatorial de {n} = {fatorial(n)}')
+# Programa Principal
+print(fatorial(5, show = True))
